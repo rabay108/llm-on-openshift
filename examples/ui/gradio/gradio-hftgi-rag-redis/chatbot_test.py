55c198097807f13e20b11a598792bc876424cba8
Twinkll Sisodia <tsisodia@redhat.com>
2023-12-11 14:24:54 -0500

    Refine script according to the changed UI

--- examples/ui/gradio/gradio-hftgi-rag-redis/chatbot_test.py
+++ examples/ui/gradio/gradio-hftgi-rag-redis/chatbot_test.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import TimeoutException
 import random
+from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Firefox()
 driver.get("https://canary-gradio-vectordb.apps.ai-dev01.kni.syseng.devcluster.openshift.com")
@@ -20,16 +21,22 @@ for user in range(10):
     element_present = EC.presence_of_element_located((By.CSS_SELECTOR, "#component-0 .scroll-hide"))
     WebDriverWait(driver, timeout).until(element_present)
 
+    Select options = new Select(driver.findElement(By.name("country")));
+
     # User enters a question
-    project_input = driver.find_element(By.CSS_SELECTOR, "#component-3 .scroll-hide")
-    project_input.clear()  # Clearing any previous input
-    project_input.send_keys(f"User {user + 1}: OpenShift AI")
-    customer_input = driver.find_element(By.CSS_SELECTOR, "#component-4 .scroll-hide")
+    customer_input = driver.find_element(By.CSS_SELECTOR, "#component-3 .scroll-hide")
     customer_input.clear()  # Clearing any previous input
-    customer_input.send_keys(f"User {user + 1}: Accenture")
-    question_input = driver.find_element(By.CSS_SELECTOR, "#component-5 .scroll-hide")
-    question_input.clear()  # Clearing any previous input
-    question_input.send_keys(f"User {user + 1}: What is OpenShift AI?")
+    customer_input.send_keys(f"User {user + 1}: Apple")
+    
+    dropdown = driver.find_element(By.CSS_SELECTOR, "#component-4 .secondary-wrap")
+    dropdown.click()  
+
+    option_to_select = driver.find_element(By.CSS_SELECTOR, "#component-4 .options, 'Red Hat Openshift Data Science'")
+    option_to_select.click()
+
+    # options = driver.find_element(By.CSS_SELECTOR, "#component-4 .options")
+    # options.click()
+
     driver.find_element(By.ID, "component-6").click()
 
     label_list=[1,1,1,1,1,1,1,1,2,3,4,5]
