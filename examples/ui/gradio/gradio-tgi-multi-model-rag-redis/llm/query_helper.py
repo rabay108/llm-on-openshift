94a27061d483dbf2bba5af77fc9abca84d9b949d
sauagarwa <sauagarw@redhat.com>
2024-03-05 17:45:10 -0500

    Fix docker build by adding missing files

--- examples/ui/gradio/gradio-tgi-multi-model-rag-redis/llm/query_helper.py
+++ examples/ui/gradio/gradio-tgi-multi-model-rag-redis/llm/query_helper.py
@@ -21,7 +21,7 @@ rds = Redis.from_existing_index(
     schema="redis_schema.yaml"
 )
 
-prompt_template = """<s>[INST] <<SYS>>
+prompt_template3 = """<s>[INST] <<SYS>>
 Instructions:
 - You are a helpful assistant in writing a project proposal for products owned by Red Hat for the company name provided in the question below.
 - Your job is to look at the  question and create the project proposal addressed to the company mentioned in question.
@@ -34,6 +34,27 @@ Instructions:
 
 Question: {question}
 Context: {context}
+
+[/INST]
+"""
+
+prompt_template = """
+### [INST]
+Instructions:
+- You are a helpful assistant in writing a project proposal for products owned by Red Hat for the company name provided in the question below.
+- Your job is to look at the  question and create the project proposal addressed to the company mentioned in question.
+- Base your answer on the provided context and question and not on prior knowledge.
+- The proposal should contain headings and sub-headings and each heading and sub-heading should be in bold.
+- Generate the project proposal in markdown language.
+- Each section in the proposal should contain only three items.
+- Proposal should be minimum of 500 lines.
+
+Here is context to help:
+{context}
+
+### QUESTION:
+Question: Generaer a project proposal for selling Red Hat OpenShift to Microsoft.
+Context: 
 
 [/INST]
 """
@@ -51,3 +72,24 @@ def get_qa_chain(llm):
         chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
         return_source_documents=True
     )
+
+prompt_template = """
+### [INST]
+Instructions:
+- You are a helpful assistant in writing a project proposal for products owned by Red Hat for the company name provided in the question below.
+- Your job is to look at the  question and create the project proposal addressed to the company mentioned in question.
+- Base your answer on the provided context and question and not on prior knowledge.
+- The proposal should contain headings and sub-headings and each heading and sub-heading should be in bold.
+- Generate the project proposal in markdown language.
+- Each section in the proposal should contain only three items.
+- Proposal should be minimum of 500 lines.
+
+Here is context to help:
+{context}
+
+### QUESTION:
+Question: Generaer a project proposal for selling Red Hat OpenShift to Microsoft.
+Context: 
+
+[/INST]
+"""
