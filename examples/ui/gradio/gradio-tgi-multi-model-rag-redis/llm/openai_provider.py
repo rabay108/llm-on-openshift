68fb224b39dfb1ee1dee7484003f8549ff9dbc41
sauagarwa <sauagarw@redhat.com>
2024-03-06 16:47:52 -0500

    Add online provider for NVIDIA

--- examples/ui/gradio/gradio-tgi-multi-model-rag-redis/llm/openai_provider.py
+++ examples/ui/gradio/gradio-tgi-multi-model-rag-redis/llm/openai_provider.py
@@ -5,7 +5,6 @@ from llm.llm_provider import LLMProvider, QueueCallback
 from queue import Queue
 import os
 
-
 class OpenAIProvider(LLMProvider):
   def __init__(self, provider, model, params):
     super().__init__(provider, model, params)
@@ -15,7 +14,7 @@ class OpenAIProvider(LLMProvider):
     print(f"[{inspect.stack()[0][3]}] Creating OpenAI LLM instance")
     try:
         #from langchain.llms import OpenAI
-        from langchain.chat_models import ChatOpenAI
+        from langchain_openai import ChatOpenAI
     except Exception as e:
         print(
             "Missing openai libraries. Openai provider will be unavailable."
@@ -26,7 +25,7 @@ class OpenAIProvider(LLMProvider):
           "base_url": self._get_llm_url("https://api.openai.com/v1"),
           "openai_api_key": self._get_llm_credentials(),
           "model": self.model,
-          "model_kwargs": {},  # TODO: add model args
+#          "model_kwargs": {},  # TODO: add model args
           "organization": None,
           "timeout": None,
           "cache": None,
